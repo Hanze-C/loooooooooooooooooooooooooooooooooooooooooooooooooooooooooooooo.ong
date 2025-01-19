aa27744790c0974ecef63a4b0c943aa4a3059136
Hanze-C <hanze2010@outlook.com>
2025-01-19 21:30:03 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const domain =
-	"loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo.ong";
+	"loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong.hanze.icu";
 export default {
-	title: "L(o*62).ong",
+	title: "L(o*59)ng.hanze.icu",
 	description: "Make your URL longer",
 	url: `https://${domain}`,
 	image: `https://${domain}/banner.png`,
