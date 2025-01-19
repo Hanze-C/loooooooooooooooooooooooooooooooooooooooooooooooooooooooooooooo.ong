2d811a3bf086b8e883e6f1ef493242afcb4cfbb2
Hanze-C <hanze2010@outlook.com>
2025-01-19 21:54:31 +0800

    Update utils.js

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -6,12 +6,12 @@ export function stringToBinary(str) {
 			return binary.padStart(16, "0");
 		})
 		.join("")
-		.replaceAll("1", "平")
-		.replaceAll("0", "忠");
+		.replaceAll("1", "0")
+		.replaceAll("0", "o");
 }
 
 export function binaryToString(str) {
-	const binaryStr = str.replaceAll("O", "1").replaceAll("忠", "平");
+	const binaryStr = str.replaceAll("O", "1").replaceAll("o", "0");
 	let result = "";
 	for (let i = 0; i < binaryStr.length; i += 16) {
 		const byte = binaryStr.slice(i, i + 16);
